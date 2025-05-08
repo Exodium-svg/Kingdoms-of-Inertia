8c038e5dd67143b9fb3caf626b14794df69a72f0
Exodium <Nem56@NEMO-WINDOWS>
2025-05-08 04:35:06 +0200

    commit before making big change

--- Kingdoms of Inertia/main.cpp
+++ Kingdoms of Inertia/main.cpp
@@ -102,7 +102,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLin
 		DeltaTimeMS deltaTimeMs = (currentTime.QuadPart - lastTime.QuadPart) * 1000 / nFrequency.QuadPart;
 		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//glCullFace(GL_BACK);
+		
 		glUseProgram(program);
 
 		manager.Update(deltaTimeMs);
